3025c4296f589610f8e6ee1adfad2c56da045fc7
Koding <ivo.baettig@gmail.com>
2015-05-19 18:50:15 +0000

    Added first grunt stuff

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: 'js/*.js',
+                dest: 'build/js/script.js'
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    banner: '// <%= grunt.template.today("dd.mm.yyyy") %> by <%= pkg.author %>\n',
+                    sourceMap: true
+                },
+                src: '<%= concat.dist.dest %>',
+                dest: 'build/js/script.min.js'
+            }
+        }
+    })
+}
---
+++ build/js/script.js
@@ -0,0 +1,9 @@
+function test1() {
+    console.log("hi test1");
+}
+
+function test2() {
+    console.log("hi test2");
+}
+test1();
+test2();
